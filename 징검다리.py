7fd6eae7b5788fe9b78d758c866937dc2c9fe4e9
KimGaYeong <rk28716@naver.com>
2021-09-03 11:34:50 +0900

    9.02

--- 징검다리.py
+++ 징검다리.py
@@ -3,24 +3,29 @@ k = 3
 
 
 def solution(stones, k):
-    answer = 1
+    answer = 0
+    left, right = 0, 200000000
+    
+    while left <= right:
+        gap = 0
+        mid = (left+right) // 2
 
-    now, gap = 0, 1
-    while (True):
-
-        if gap == k:
-            break
+        for stone in stones:
+            if stone - mid <= 0:
+                gap +=1
+                if gap ==k:
+                    gap = k+len(stones)
+                    break
+            else:
+                gap =0
         
-        if now == len(stones):
-            now =0
-            answer +=1
+        if gap == k+len(stones):
+            right = mid-1
+        else:
+            left = mid+1
+            answer = left
 
-        if stones[now] == 0:
-            now +=1
-            gap +=1
-        elif stones[now] != 0:
-            stones[now] -=1
-            now +=1
+    print(answer)
     return answer
 
 solution(stones, k)
